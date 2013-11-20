c100d54ff12864ab483a23ea229e07c249d7267e
Richard Walker <richard@aqume.com>
2013-11-19 22:13:56 -0800

    Working on getting postgres to like the translated mysql sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,12 +2,11 @@
  
 # --- !Ups
 
-SET sql_mode = 'NO_ZERO_DATE';
-
+CREATE SEQUENCE quiz_seq;
 CREATE TABLE quiz (
-    id INT NOT NULL AUTO_INCREMENT,
+    id integer NOT NULL DEFAULT nextval('quiz_seq'),
     title varchar(60),
-    retries INT,
+    retries integer,
     drag BOOLEAN,
     instructions TEXT,
     created TIMESTAMP NULL,
@@ -15,114 +14,120 @@ CREATE TABLE quiz (
     record_link TEXT,
     PRIMARY KEY (id)
 );
+CREATE SEQUENCE quest_par_format_seq;
 CREATE TABLE quest_par_format (
-    id INT NOT NULL AUTO_INCREMENT,
-    format VARCHAR(20) NOT NULL,
+    id integer NOT NULL DEFAULT nextval('quest_par_format_seq'),
+    format varchar(20) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE question_par (
-    quiz_id INT NOT NULL,
-    quest_num INT NOT NULL,
+    quiz_id integer NOT NULL,
+    quest_num integer NOT NULL,
     quest_text TEXT,
-    format_id INT,
+    format_id integer,
     PRIMARY KEY (quiz_id, quest_num),
-    INDEX quiz_ind (quiz_id),
+    --INDEX quiz_ind (quiz_id),
     FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE,
-    INDEX format_ind (format_id),
+    --INDEX format_ind (format_id),
     FOREIGN KEY (format_id) REFERENCES quest_par_format(id)
 );
 CREATE TABLE correct_answer (
-	quiz_id INT NOT NULL,
-	quest_num INT NOT NULL,
-	word_ind INT NOT NULL,
-	corr_ans VARCHAR(45),
+	quiz_id integer NOT NULL,
+	quest_num integer NOT NULL,
+	word_ind integer NOT NULL,
+	corr_ans varchar(45),
 	PRIMARY KEY (quiz_id, quest_num, word_ind),
-	INDEX quiz_ind (quiz_id),
+	--INDEX quiz_ind (quiz_id),
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
 );
+CREATE SEQUENCE draggable_seq;
 CREATE TABLE draggable (
-    id INT NOT NULL AUTO_INCREMENT,
-    quiz_id INT NOT NULL,
-    draggable VARCHAR(45),
-    disp_order INT,
+    id integer NOT NULL DEFAULT nextval('draggable_seq'),
+    quiz_id integer NOT NULL,
+    draggable varchar(45),
+    disp_order integer,
     PRIMARY KEY (id),
-    INDEX quiz_ind (quiz_id),
+    --INDEX quiz_ind (quiz_id),
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
 );
+CREATE SEQUENCE role_seq;
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    role VARCHAR(20) UNIQUE NOT NULL,
+    id integer NOT NULL DEFAULT nextval('role_seq'),
+    "role" varchar(20) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE SEQUENCE user_seq;
 CREATE TABLE user (
-	id INT NOT NULL AUTO_INCREMENT,
-	user_name VARCHAR(25) UNIQUE,
-	first_name VARCHAR(45),
-	last_name VARCHAR(45),
-	password VARCHAR(20) NOT NULL,
-	role VARCHAR(20) NOT NULL,
-	email VARCHAR(60),
+	id integer NOT NULL DEFAULT nextval('user_seq'),
+	user_name varchar(25) UNIQUE,
+	first_name varchar(45),
+	last_name varchar(45),
+	password varchar(20) NOT NULL,
+	"role" varchar(20) NOT NULL,
+	email varchar(60),
     created TIMESTAMP NULL,
     modified TIMESTAMP NULL,
 	PRIMARY KEY (id)
 );
+CREATE SEQUENCE course_seq;
 CREATE TABLE course (
-    id INT NOT NULL AUTO_INCREMENT,
-	course_num VARCHAR(45),
-	title VARCHAR(60),
-	section VARCHAR(20),
-	semester VARCHAR(20),
-	year VARCHAR(4),
+    id integer NOT NULL DEFAULT nextval('course_seq'),
+	course_num varchar(45),
+	title varchar(60),
+	section varchar(20),
+	semester varchar(20),
+	year varchar(4),
     created TIMESTAMP NULL,
     modified TIMESTAMP NULL,
 	PRIMARY KEY (id)
 );
 CREATE TABLE quiz_course (
-	quiz_id INT NOT NULL,
-	course_id INT NOT NULL,
+	quiz_id integer NOT NULL,
+	course_id integer NOT NULL,
 	PRIMARY KEY (quiz_id, course_id),
-	INDEX quiz_ind (quiz_id),
+	--INDEX quiz_ind (quiz_id),
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE,
-	INDEX course_ind (course_id),
+	--INDEX course_ind (course_id),
 	FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
 );
+CREATE SEQUENCE unit_seq;
 CREATE TABLE unit (
-	id INT NOT NULL AUTO_INCREMENT,
-	course_id INT NOT NULL,
-	title VARCHAR(60),
+	id integer NOT NULL DEFAULT nextval('unit_seq'),
+	course_id integer NOT NULL,
+	title varchar(60),
 	descrip TEXT,
-	schedule VARCHAR(45),
+	schedule varchar(45),
 	PRIMARY KEY (id)
 );
 CREATE TABLE quiz_unit (
-	quiz_id INT NOT NULL,
-	unit_id INT NOT NULL,
+	quiz_id integer NOT NULL,
+	unit_id integer NOT NULL,
 	PRIMARY KEY (quiz_id, unit_id),
-	INDEX quiz_ind (quiz_id),
+	--INDEX quiz_ind (quiz_id),
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE,
-	INDEX unit_ind (unit_id),
+	--INDEX unit_ind (unit_id),
 	FOREIGN KEY (unit_id) REFERENCES unit(id) ON DELETE CASCADE
 );
 CREATE TABLE user_course (
-	user_id INT NOT NULL,
-	course_id INT NOT NULL,
+	user_id integer NOT NULL,
+	course_id integer NOT NULL,
 	PRIMARY KEY (user_id, course_id),
-	INDEX user_ind (user_id),
+	--INDEX user_ind (user_id),
 	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
-	INDEX course_ind (course_id),
+	--INDEX course_ind (course_id),
 	FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
 );
 CREATE TABLE quiz_attempt (
-	user_id INT NOT NULL,
-	quiz_id INT NOT NULL,
-	attemptNum INT NOT NULL,
-	score VARCHAR(20),
+	user_id integer NOT NULL,
+	quiz_id integer NOT NULL,
+	attemptNum integer NOT NULL,
+	score varchar(20),
 	taker_ans TEXT,
 	date_time DATETIME,
 	PRIMARY KEY (user_id, quiz_id, attemptNum),
-	INDEX user_ind (user_id),
+	--INDEX user_ind (user_id),
 	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
-	INDEX quiz_ind (quiz_id),
+	--INDEX quiz_ind (quiz_id),
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
 );
 
@@ -141,4 +146,13 @@ DROP TABLE correct_answer;
 DROP TABLE question_par;
 DROP TABLE quest_par_format;
 DROP TABLE quiz;
+
+DROP SEQUENCE quiz_seq;
+DROP SEQUENCE quest_par_format_seq;
+DROP SEQUENCE draggable_seq;
+DROP SEQUENCE role_seq;
+DROP SEQUENCE user_seq;
+DROP SEQUENCE course_seq;
+DROP SEQUENCE unit_seq;
+
 
